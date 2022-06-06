84773aa150322e6816d7093fb0fd87d64b91a11b
oyj <supercode@outlook.sg>
2022-06-07 00:33:28 +0800

    不同环境下的webpack的mode output配置

--- webpack/utils.js
+++ webpack/utils.js
@@ -1,3 +1,11 @@
 const path = require('path');
 
+const env = process.env.NODE_ENV;
+
 exports.getPath = (dir) => path.join(__dirname, '..', dir);
+
+exports.IS_PROD = env === 'production';
+
+exports.IS_DEV = env === 'development';
+
+exports.IS_TEST = env === 'test';
--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -1,10 +1,16 @@
-const { getPath } = require('./utils');
+const { getPath, IS_DEV } = require('./utils');
 
 module.exports = {
   mode: 'production',
   entry: getPath('src/index.ts'),
   output: {
-    filename: 'main.js',
     path: getPath('dist'),
+    filename: IS_DEV
+      ? 'js/[name].bundle.js'
+      : 'js/[name].[contenthash:8].bundle.js',
+    chunkFilename: IS_DEV
+      ? 'js/[name].bundle.js'
+      : 'js/[name].[contenthash:8].bundle.js',
+    clean: true,
   },
 };
